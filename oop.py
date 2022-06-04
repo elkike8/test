e2d9318262784bd432ffb636a5aabf8bd2a88e5c
Fer Gonzalez <fergonzalez16@gmail.com>
2022-06-04 17:00:31 +0200

    added age check function

--- oop.py
+++ oop.py
@@ -18,14 +18,41 @@ class Person:
 
         self.age = age
 
+    def speak(self):
+        print(f"I am a person, my name is {self.name}")
+
     def say_im_cool(self):
         print(f"damn {self.name}, you are a cool cat for being {self.age} years old")
 
 
-fer = Person()
-fer.set_name("fer")
-fer.set_age("ham")
-fer.say_im_cool()
+class Latino(
+    Person
+):  # inheritance, all the functions of the class in parenthesis are available, but the functions defined here override the parent ones
+    def speak(self):
+        print(f"I am latino, my name is {self.name} and I'm {self.age} years old")
 
+
+fer = Latino()
+fer.set_name("fer")
 fer.set_age(33)
-print(fer.age)
+fer.speak()
+
+kaja = Person()
+kaja.set_name("kaja")
+kaja.set_age(38)
+kaja.speak()
+
+
+class Course:
+    def __init__(self, name, max_students):
+        self.name = name
+        self.max_students = max_students
+        self.students = []
+
+    def add_student(self, student):  # the student is an instance of the student object
+        if len(self.students) < self.max_students:
+            self.students.append(student.name())
+            print(f"{student} added into the course {self.name}")
+        else:
+            print(f"max student exceeded, couldn't add {student.name}")
+
